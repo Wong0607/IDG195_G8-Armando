9a569a1f8dd89071d5041d8a62092903425d08bc
Wong0607 <armandof.w@gmail.com>
2022-02-18 21:44:10 -0800

    description

---
+++ Operaciones.py
@@ -0,0 +1,10 @@
+Num1 = input("Ingresa un numero: ")
+Num2 = input("Ingresa otro numero: ")
+Respuesta = float(Num1) + float(Num2)
+print("La respuesta de la suma es:", Respuesta)
+
+Num3 =  input("Ingresa un numero: ")
+Num4 = input ("Ingresa otro numero: ")
+Respuesta2 = float(Num3) - float(Num4)
+print("La respuesta de la resta es: ", Respuesta2)
+
